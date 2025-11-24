726299fd03856bb0cfacb5cda2aa252fabe45d96
moldovanluca <moldovanluca1605@gmail.com>
2025-11-24 21:49:14 +0100

    python script to see the conetnt of companies_discussions table

---
+++ view_companies_discussions.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def view_db():
+    conn = sqlite3.connect('companies_url.db')
+    cursor = conn.cursor()
+    sql_query = "SELECT * FROM companies_discussions"
+    
+    try:
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        print(f"Found {len(rows)} entries")
+        for row in rows:
+            print(row)
+            
+    except sqlite3.OperationalError as e:
+        print(f"Error: {e}")
+        tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+        print(f"Tables found: {tables}")
+
+    conn.close()
+
+if __name__ == "__main__":
+    view_db()
